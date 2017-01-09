e3dd2279ce22b74ea5e34d215535292dc1cf3495
Than Fox <magnumtf@yahoo.com>
2017-01-08 20:39:32 -0500

    Initial Version

---
+++ app/src/main/java/com/buber/hellofarts/HelloFartsActivity.java
@@ -0,0 +1,13 @@
+package com.buber.hellofarts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloFartsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_farts);
+    }
+}
